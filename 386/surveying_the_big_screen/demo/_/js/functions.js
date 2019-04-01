71e0844d1716d28f47c4dc74b0ac2908264eb51d
sanketio <sanket.parmar@rtcamp.com>
2019-04-01 17:29:08 +0530

    Add files for legacy_articles-15778

---
+++ 386/surveying_the_big_screen/demo/_/js/functions.js
@@ -0,0 +1,8 @@
+// Browser detection for when you get desparate. A measure of last resort.
+// http://rog.ie/post/9089341529/html5boilerplatejs
+
+// var b = document.documentElement;
+// b.setAttribute('data-useragent',  navigator.userAgent);
+// b.setAttribute('data-platform', navigator.platform);
+
+// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
